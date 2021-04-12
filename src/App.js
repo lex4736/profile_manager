1a9ca72893b8453fe6f4c4cba91dd8a405916f8b
Alexis Arando <lex4736@gmail.com>
2021-04-11 21:51:30 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload Alexis Arando.
+          Edit <code>src/App.js</code> and save to reload Alexis Arando rytrtyrtyr.
         </p>
         <a
           className="App-link"
